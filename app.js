1f7741c534dc7507f379d76d6d67d285f441f40c
Byungil Park <rtlink@hanmail.net>
2012-04-09 20:51:38 +0900

    4/10 Update

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'secret is none' }));
+  app.use(express.logger({ format: '\x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m :response-time ms' }))
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
@@ -36,6 +37,8 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+
+// 데이터모델 정의
 //--------------------------------------------------------------------------------------------//
 models.defineModels(mongoose, function() {
     app.User = User = mongoose.model('User');
@@ -43,6 +46,7 @@ models.defineModels(mongoose, function() {
     db = mongoose.connect(app.set('db-uri'));
 })
 
+// 유저 찾기
 //--------------------------------------------------------------------------------------------//
 function loadUser(req, res, next) {
     if (req.session.user_id) {
@@ -51,13 +55,16 @@ function loadUser(req, res, next) {
                 req.currentUser = user;
                 next();
             } else {
-                res.redirect('/sessions/new');
+                console.log('not found............1!!!');
+                res.redirect('/signin');
             }
         });
     } else if (req.cookies.logintoken) {
+        console.log('login by token');
         authenticateFromLoginToken(req, res, next);
     } else {
-        res.redirect('/sessions/new');
+        console.log('not found!!!');
+        res.redirect('/signin');
     }
 }
 
@@ -72,29 +79,45 @@ app.get('/', loadUser, function(req, res){
 });
 app.get('/list', routes.list);
 
+// 로그인
 //--------------------------------------------------------------------------------------------//
-// Sessions
-app.get('/sessions/new', function(req, res) {
+app.get('/signin', function(req, res) {
     res.render('index.jade', {
         locals: { user: new User() }
     });
 });
 
+// 신규사용자
 //--------------------------------------------------------------------------------------------//
+app.get('/users/new', function(req, res) {
+    res.render('new.jade', {
+        locals: { user: new User() }
+    });
+});
+
+
 // 유저 추가
+//--------------------------------------------------------------------------------------------//
 app.post('/users.:format?', function(req, res) {
     var user = new User(req.body.user);
 
     function userSaveFailed() {
-        req.flash('error', 'Account creation failed');
-        res.render('index.jade', {
-            locals: { user: user }
-        });
+        req.session.user_id = user.id;
+        res.render('showuser', {
+                user: user
+                , title: 'Express'
+            }
+        );
     }
 
     user.save(function(err) {
-        if (err) return userSaveFailed();
-        req.flash('info', 'Your account has been created');
+        if (err) {
+            console.log('저장실패');
+            return userSaveFailed();
+        } else {
+            console.log('저장OK');
+            req.flash('info', 'Your account has been created');
+        }
 
         switch (req.params.format) {
             case 'json':
@@ -113,41 +136,46 @@ app.post('/users.:format?', function(req, res) {
 });
 
 
+// 로그인
 //--------------------------------------------------------------------------------------------//
 app.post('/sessions', function(req, res) {
+    console.log('로그인 : %s', req.body.user.email);
     User.findOne({ email: req.body.user.email }, function(err, user) {
         if (user && user.authenticate(req.body.user.password)) {
             req.session.user_id = user.id;
 
-            // Remember me
             if (req.body.remember_me) {
+                // 쿠키에 사용자 저장
                 var loginToken = new LoginToken({ email: user.email });
                 loginToken.save(function() {
                     res.cookie('logintoken', loginToken.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/' });
-                    res.redirect('/documents');
+                    res.redirect('/');
                 });
             } else {
-                res.redirect('/documents');
+                res.redirect('/');
             }
         } else {
-            req.flash('error', 'Incorrect credentials');
-            res.redirect('/sessions/new');
+            // 사용자가 없거나 패스워드 오류
+            res.render('notfound.jade');
         }
     });
 });
 
+// 로그아웃
+//--------------------------------------------------------------------------------------------//
 app.del('/sessions', loadUser, function(req, res) {
+    console.log('로그아웃');
     if (req.session) {
         LoginToken.remove({ email: req.currentUser.email }, function() {});
         res.clearCookie('logintoken');
         req.session.destroy(function() {});
     }
-    res.redirect('/sessions/new');
+    res.redirect('/signin');
 });
 
 
 
+// 서버 스타트
 //--------------------------------------------------------------------------------------------//
-
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
